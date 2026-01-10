4c5bb0060c24c19ba437c60e39f404f34d3f9c07
Spencer337 <spencer.m.walter@gmail.com>
2026-01-10 13:20:01 -0500

    Added a rotation action task to the rotate state, the plant rotates 360 clockwise before the action ends.

---
+++ Assets/Scripts/RotateAT.cs
@@ -0,0 +1,46 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class RotateAT : ActionTask {
+		public float t = 0;
+		public float rotationSpeed = 30;
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+            t -= Time.deltaTime * rotationSpeed;
+			agent.transform.eulerAngles = new Vector3(0.0f, 0.0f, t);
+			if (t <= -360)
+			{
+				agent.transform.eulerAngles = Vector3.zero;
+				t = 0;
+				EndAction();
+            }
+		}
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
