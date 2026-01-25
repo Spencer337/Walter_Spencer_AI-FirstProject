ba529ad073a4557cb01e0ad27f813c8513d0d91c
Spencer337 <spencer.m.walter@gmail.com>
2026-01-25 18:00:31 -0500

    Implemented the walking within a sub FSM of the astronaut's FSM, the player moves in 4 directions based on the arrow key pressed, and accelerates and decelerates.

---
+++ Assets/Scripts/WalkAT.cs
@@ -0,0 +1,82 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class WalkAT : ActionTask {
+        public BBParameter<Vector3> velocity;
+		public Vector3 playerInput;
+		public float acceleration;
+		public float deceleration;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+			//float height = agent.transform.position.y;
+			//velocity.value = agent.transform.position;
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+			if (Input.GetKey(KeyCode.RightArrow))
+			{
+				playerInput = Vector3.right;
+			}
+			else if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				playerInput = Vector3.left;
+			}
+			else if (Input.GetKey(KeyCode.UpArrow))
+			{
+				playerInput = Vector3.forward;
+			}
+			else if (Input.GetKey(KeyCode.DownArrow))
+			{
+				playerInput = Vector3.back;
+			}
+			else
+			{
+				playerInput = Vector3.zero;
+			}
+			if (playerInput.magnitude > 0)
+			{
+                velocity.value = playerInput * acceleration * Time.deltaTime;
+            }
+			else
+			{
+                Vector3 changeInVelocity = velocity.value.normalized * deceleration * Time.deltaTime;
+                // If the player is going to overshoot their deceleration, set horizontal velocity to zero
+                if (changeInVelocity.magnitude > velocity.value.magnitude)
+                {
+                    velocity.value = Vector3.zero;
+                }
+                // Otherwise, decelerate normally
+                else
+                {
+                    velocity.value -= changeInVelocity;
+                }
+            }
+			agent.transform.position += velocity.value;
+        }
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
