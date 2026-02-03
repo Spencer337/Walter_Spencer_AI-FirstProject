f187958d792548143702a5cdc2f2a78164d65cc2
Spencer Walter <waltespe@shernet.sheridancollege.ca>
2026-02-02 22:09:09 -0500

    Experimented with the parrot flapping it's wings in the fly state. The parrot's wings successfully rotate up and down while in the fly state.

---
+++ Assets/Scripts/Action Tasks/FlapWingsAT.cs
@@ -0,0 +1,48 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class FlapWingsAT : ActionTask {
+		public BBParameter<Transform> leftWingPivot;
+        public BBParameter<Transform> rightWingPivot;
+		public float maxRotation, minRotation;
+		public float rotationSpeed;
+		private float r;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+            r += Time.deltaTime * rotationSpeed;
+			leftWingPivot.value.transform.eulerAngles = new Vector3(0, 0, r);
+            rightWingPivot.value.transform.eulerAngles = new Vector3(0, 0, -r);
+			if (r > maxRotation || r < minRotation)
+			{
+				rotationSpeed = -rotationSpeed;
+			}
+        }
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
